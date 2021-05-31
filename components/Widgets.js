5f49548d898b97f5650c404fa38a0b9be244a04d
Gianluca Andretta <25036072+gnlca@users.noreply.github.com>
2021-06-01 01:05:38 +0200

    passo al desktop

--- components/Widgets.js
+++ components/Widgets.js
@@ -11,8 +11,8 @@ const Widgets = props => {
             <div className="griglia">
 
                 <div className="cpu  extendedFont">cpu<CpuTemp temperature={props.temperature}/></div>
-                <div className="cpu  extendedFont" > freeRam<br/><span style={{fontSize:"x-large"}}>1338MB</span></div>
-                <div className="cpu  extendedFont">hostname<br/><span style={{fontSize:"x-large"}}>agpi</span></div>
+                <div className="cpu  extendedFont" > freeRam<br/><span className="widgettino">1338MB</span></div>
+                <div className="cpu  extendedFont">hostname<br/><span className="widgettino">agpi</span></div>
 
             </div>
             <div className="cpu  extendedFont" >version<br/><span style={{}}>Linux version 5.10.17-v7l+ (dom@buildbot) (arm-linux-gnueabihf-gcc-8 (Ubuntu/Linaro 8.4.0-3ubuntu1) 8.4.0, GNU ld (GNU Binutils for Ubuntu) 2.34) #1414 SMP Fri Apr 30 13:20:47 BST 2021</span></div>
