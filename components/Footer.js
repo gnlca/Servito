3c6321f2e593f2f2b7b787253ebb1490d82bff0a
gnlca <25036072+gnlca@users.noreply.github.com>
2021-06-01 11:08:55 +0200

    passo al portatile
    aggiunto _document.js per sovrascrivere il modello Document di default di next.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,12 +4,12 @@ import React from 'react';
 function Footer() {
 
 
-    var today = new Date();
-    var time = today.getDate() + "." + today.getMonth() + "."  + today.getFullYear();
+    // var today = new Date();
+    // var time = today.getDate() + "." + today.getMonth() + "."  + today.getFullYear();
 
     return(<div className="Footer">
         <footer>
-        <span style={{fontFamily:"Esteso", fontSize:"small"}}>{time}</span>
+        <span style={{fontFamily:"Esteso", fontSize:"small"}}>v0.01</span>
         </footer>
         
     </div>)
---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+            <meta charSet="utf-8" />    
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0"/>
+            <meta name="apple-mobile-web-app-capable" content="yes"/>
+            <title>agpi</title>
+            <link rel="apple-touch-icon" href="/assets/img/favicon.jpg"/>
+            <link rel="icon" type="image/x-icon" href="/assets/img/favicon.ico"/>
+            {/* <link rel="manifest" href="%PUBLIC_URL%/manifest.json" /> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
